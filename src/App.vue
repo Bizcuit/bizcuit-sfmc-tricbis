0be2bfad78993329b5a3bcf1ad942c75f9c0cb09
Sergey Agadzhanov <agadzhanov@gmail.com>
2022-04-28 13:58:26 +0200

    Template alignment with the docs + Minor UI changes + Price Locale configuration

--- src/App.vue
+++ src/App.vue
@@ -12,25 +12,24 @@
             </div>
         </div>
 
-        <div class="columns">
-            <div class="column">
-                <div class="field">
-                    <label class="label is-small">Field Name:</label>
-                    <div class="control">
-                        <div class="select is-small">
-                            <select v-model="content.field">
-                                <option value="Trigger_Catalog_Items">
-                                    Trigger Catalog Items
-                                </option>
-                                <option value="Recommendations">Recommendations</option>
-                                <option value="Additional_Recommendations">
-                                    Additional Recommendations
-                                </option>
-                            </select>
-                        </div>
-                    </div>
+        <div class="field">
+            <label class="label is-small">Field Name:</label>
+            <div class="control">
+                <div class="select is-small">
+                    <select v-model="content.field">
+                        <option value="Trigger_Catalog_Items">
+                            Trigger Catalog Items
+                        </option>
+                        <option value="Recommendations">Recommendations</option>
+                        <option value="Additional_Recommendations">
+                            Additional Recommendations
+                        </option>
+                    </select>
                 </div>
             </div>
+        </div>
+
+        <div class="columns is-mobile">
             <div class="column">
                 <div class="field">
                     <label class="label is-small">Block Width:</label>
@@ -44,9 +43,22 @@
                     </div>
                 </div>
             </div>
+            <div class="column">
+                <div class="field">
+                    <label class="label is-small">Currency Locale</label>
+                    <div class="control">
+                        <input
+                            class="input is-small"
+                            type="text"
+                            placeholder="e.g. us-EN"
+                            v-model="content.currencyLocale"
+                        />
+                    </div>
+                </div>                
+            </div>
         </div>
 
-        <div class="columns">
+        <div class="columns is-mobile">
             <div class="column">
                 <div class="field">
                     <label class="label is-small">Items per row:</label>
@@ -95,9 +107,10 @@ export default {
             },
             content: {
                 field: "Recommendations",
+                currencyLocale: "en-US",
                 headline: "",
                 numberOfItemsPerRow: 3,
-                numberOfRows: 2,
+                numberOfRows: 1,
                 blockWidth: 600,
             },
         };
@@ -123,68 +136,38 @@ export default {
                 "]".toLowerCase() + "%".toLowerCase() + "%".toLowerCase();
 
             return `
-            <table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" bgcolor="#ffffff">
-                <tr>
-                    <td>
-                        <table align="center" border="0" cellpadding="0" cellspacing="0" width="${this.content.blockWidth}">
-                            <tr>
-                                <td width="100%">
-                                    <!-- IS TRIGGERS GTL: START -->
-
-                                        ${ampOpen}
-                                            SET @Recommendations = AttributeValue("${this.content.field}")
-                                        ${ampClose}
-
-                                        <table cellpadding="0" cellspacing="0" width="100%" role="presentation" style="min-width: 100%;">
-                                            <tr>
-                                                <td class="stylingblock-content-wrapper camarker-inner">
-
-                                                    <div style="display: block; text-align: center; font-size: 20px; font-weight: bold; color: #555555; line-height: 30px; font-family: arial;">
-                                                        ${this.content.headline}
-                                                    </div>
-
-                                                    <!-- start evergage recs block (Recs) -->
-                                                    <table cellspacing="0" cellpadding="0" border="0" align="center" width="100%" style="table-layout: fixed; margin-top: 1.5rem;">
-
-                                                        <tr>
-                                                            <td width="100%">
-                                                                <div class="evergage-block" style="max-height: 100%; max-width: 100%; position: relative; overflow: hidden; text-align: center; ">
-                                                                    <div class="evergage-recs" style="display: table; width: 100%; max-height: 100%;">
-                                                                        {{.datasource JSONVar type=variable maxRows=${maxNumberOfItems}}}
-                                                                        {{.data}}
-                                                                        { "target" : "@Recommendations" }
-                                                                        {{/data}}
-
-                                                                        <span style="display: inline-block; margin-top: 10px;">
-                                                                            <a href="{{url}}" style="display: block; width: ${width}px; height: ${width}px; overflow: hidden;">
-                                                                                <img src="{{imageUrl}}" style="border: 0; width: 100%;" border="0" alt="" />
-                                                                            </a>
-                                                                            <div style="width: ${width}px; max-width: 100%;">
-                                                                                <p style="margin: 5px 0px; height: 32px; overflow: hidden; font-weight: bold; font-family: arial; font-size: 14px; color: #555555;">{{name}}</p>
-                                                                                <p style="margin: 5px 0px; font-family: arial; font-size: 12px; color: #555555;">Price: {{=FormatCurrency(price, "en-US")}}</p>
-                                                                            </div>
-                                                                        </span>
-
-                                                                        {{/datasource}}
-
-                                                                    </div>
-                                                                </div>
-                                                            </td>
-                                                        </tr>
-
-                                                    </table>
-                                                    <!-- end evergage recs block (Recs) -->
-                                                </td>
-                                            </tr>
-                                        </table>
-
-                                    <!-- IS TRIGGERS GTL: FINISH -->
-                                </td>
-                            </tr>
-                        </table>
-                    </td>
-                </tr>
-            </table>
+            ${ampOpen}
+                SET @Catalog_Data = AttributeValue("${this.content.field}")
+            ${ampClose}
+            
+            <div style="max-height: 100%; max-width: ${this.content.blockWidth}px; position: relative; overflow: hidden; margin: 20px auto; text-align: center;">
+                <div style="display: block; text-align: center; font-size: 20px; font-weight: bold; color: #555555; line-height: 30px; font-family: arial;">
+                    ${this.content.headline}
+                </div>
+        
+                <div style="display: table; width: 100%; max-height: 100%;">
+                    {{.datasource JSONVar type=variable maxRows=${maxNumberOfItems}}}
+                    {{.data}}
+                        { "target" : "@Catalog_Data" }
+                    {{/data}}
+            
+                    <span style="display: inline-block; margin-top: 10px;">
+                        <a href="{{url}}" style="display: block; width: ${width}px; overflow: hidden;">
+                            <img src="{{imageUrl}}" style="border: 0; width: 90%; margin: 10px;">
+                        </a>
+                        <div style="padding: 0px; margin: 0px; width: ${width}px; overflow: hidden;">
+                            <h3 style="padding: 0px; margin: 10px; font-size: 16px; line-height: 20px; height: 40px; overflow: hidden;">
+                                {{name}}
+                            </h3>
+                            <p style="padding: 0px; margin: 10px; font-size: 14px;">
+                                Price: {{=FormatCurrency(price, "${this.content.currencyLocale}")}}
+                            </p>
+                        </div>
+                    </span>
+                    
+                    {{/datasource}}
+                </div>
+            </div>
             `;
         },
 
@@ -248,4 +231,7 @@ export default {
     padding: 20px;
     max-width: 500px;
 }
+
+div.select { width: 100%; }
+select { width: 100%; }
 </style>
